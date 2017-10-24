3a7511652166a1ffc439f602f6b207c22a3d1cea
cesalexguz <cesalexguz@outlook.com>
2017-10-23 19:04:58 -0500

    Add files via upload

---
+++ Assets/Scrpts/Trigerjump.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigerjump : MonoBehaviour {
+
+	public Animator anim;
+
+	void OnCollisionEnter()
+	{
+		anim.SetBool("Jumptrig", true);
+	}
+	void OnCollisionExit()
+	{
+		anim.SetBool("Jumptrig", false);
+	}	
+}
